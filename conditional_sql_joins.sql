e028a9700fafa74308c98176e9af34adae0d9c49
venkat9940 <52745987+venkat9940@users.noreply.github.com>
2025-07-10 12:51:33 -0500

    Update conditional_sql_joins.sql

--- conditional_sql_joins.sql
+++ conditional_sql_joins.sql
@@ -4,3 +4,12 @@ from goodreads as g
 join orders as o 
 on g.book_id = o.book_id
 and o.quantity>2;
+
+-- Retrieve order IDs and their corresponding delivery status where the delivery status is either 'Delivered' or 'Shipped'.
+select 
+o.order_id,
+d.delivery_status
+from orders o 
+join deliveries d 
+on o.order_id = d.order_id
+and d.delivery_status in ('Delivered','Shipped');
